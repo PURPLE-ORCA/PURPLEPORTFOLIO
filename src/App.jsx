27e37f42891d2963e059b09ceeea862626fd1f51
PURPLE-ORCA <medms2005@gmail.com>
2025-05-30 18:00:51 +0100

    One shot initial design and folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import { useState } from 'react'
-import './App.css'
+import Home from './pages/Home';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-     
-    </>
-  )
+    <Home />
+  );
 }
 
-export default App
+export default App;
